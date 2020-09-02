901daaae2ac76b24cadb71f5897f636538d40313
User <User@User-PC>
2020-09-02 10:42:13 +0530

    Google account pages

---
+++ googleaccount/src/main/java/com/busysuit/GoogleaccountApplication.java
@@ -0,0 +1,13 @@
+package com.busysuit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleaccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleaccountApplication.class, args);
+	}
+
+}
